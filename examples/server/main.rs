5bc1f5e9180330dd0a5f20db5821b09c65c61605
Benjamin O <jeep70cp@gmail.com>
2019-06-01 20:48:31 -0400

    This is a base that needs some serious refining

---
+++ examples/server/main.rs
@@ -0,0 +1,125 @@
+#![feature(await_macro, async_await)]
+
+use libsynchro;
+
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use std::net::SocketAddr;
+
+use futures::sync::mpsc;
+
+use bytes::{Bytes, BytesMut, IntoBuf, Buf, BufMut};
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio::codec::{Framed, BytesCodec, Decoder};
+use tokio::prelude::stream::*;
+use tokio::prelude::*;
+
+type ClientListArc = Arc<Mutex<HashMap<SocketAddr, mpsc::UnboundedSender<Bytes>>>>;
+
+struct Client {
+    stream: SplitStream<Framed<TcpStream, BytesCodec>>,
+    addr: SocketAddr,
+    client_list: ClientListArc,
+}
+
+impl Client {
+    fn new(socket: TcpStream, client_list: ClientListArc) -> Self {
+        let addr = socket.peer_addr().unwrap();
+        println!("Client connected: {}", addr);
+
+        let framed = BytesCodec::new().framed(socket);
+        let (mut sink, stream) = framed.split();
+
+        let (sender, mut reciever) = mpsc::unbounded();
+        client_list.lock().unwrap().insert(addr, sender);
+
+        // When data is sent to this Client object through the
+        // mpsc system, send it to the actual client
+        tokio::spawn_async(async move { 
+            while let Some(data) = await!(reciever.next()) {
+                let data = data.unwrap();
+                println!("{:?}", data);
+                await!(sink.send_async(data)).unwrap();
+            }
+        });
+
+        Client {
+            stream,
+            addr,
+            client_list,
+        }
+    }
+
+    async fn recieve(&mut self) { 
+        let mut buffer = BytesMut::new();
+        let mut anticipated_message_length: u16 = 0;
+        while let Some(message) = await!(self.stream.next()) {
+            // Add newly received information to the buffer
+            buffer.unsplit(message.unwrap());
+            
+            // Retrieve message length if we did'nt already
+            if anticipated_message_length == 0 {
+                if buffer.len() < 2 {
+                    continue;
+                }
+                
+                anticipated_message_length = buffer.split_to(2).into_buf().get_u16_be();
+
+                println!("message length: {}", anticipated_message_length);
+            }
+
+            // Process the rest of the message if we're sure that we have the entire thing
+            if buffer.len() >= anticipated_message_length as usize {
+                let split_bytes = buffer.split_to(anticipated_message_length as usize);
+                let mut header_bytes = BytesMut::with_capacity(2);
+                header_bytes.put_u16_be(anticipated_message_length);
+                header_bytes.unsplit(split_bytes.clone());
+
+                let mut clients = self.client_list.lock().unwrap();
+                for (peer_addr, sender) in clients.iter_mut() {
+                    if *peer_addr != self.addr {
+                        sender.unbounded_send(Bytes::from(header_bytes.clone())).unwrap();
+                    }
+                }
+
+                anticipated_message_length = 0;
+                buffer.clear(); // remember to remove this
+            }
+        }
+    }
+}
+
+impl Drop for Client {
+    fn drop(&mut self) {
+        println!("Dropped client {}", self.addr);
+        self.client_list.lock().unwrap().remove(&self.addr);
+    }
+}
+
+fn main() {
+    let listener = start_server(32019).unwrap();
+    println!("Server started sucessfully on {}", listener.local_addr().unwrap());
+
+    let client_list: ClientListArc = Arc::new(Mutex::new(HashMap::new()));
+
+    tokio::run_async(async move {
+        let mut incoming = listener.incoming();
+
+        while let Some(stream) = await!(incoming.next()) {
+            let stream = stream.unwrap();
+
+            let mut client = Client::new(stream, client_list.clone());
+
+            tokio::spawn_async(async move { 
+                await!(client.recieve()); 
+            });
+        }
+    });
+}
+
+fn start_server(port: u16) -> Result<TcpListener, Box<std::error::Error>> {
+    let addr: SocketAddr = format!("0.0.0.0:{}", port).parse()?; // to-do: add ipv6
+    let listener = TcpListener::bind(&addr)?;
+    Ok(listener)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,98 @@
+#![feature(await_macro, async_await)]
+
+use std::net::SocketAddr;
+
+use bytes::{BytesMut, IntoBuf, Buf};
+
+use tokio::net::TcpStream;
+use tokio::codec::{Framed, BytesCodec, Decoder};
+use tokio::prelude::*;
+
+#[derive(Debug, Hash, PartialEq, Eq)]
+pub enum Command {
+    Invalid = 0,
+    Pause = 1,
+    Seek = 2,
+}
+
+impl From<u8> for Command {
+    fn from(num: u8) -> Self {
+        match num {
+            1 => Command::Pause,
+            2 => Command::Seek,
+            _ => Command::Invalid,
+        }
+    }
+}
+
+pub struct SynchroConnection {
+    framed: Framed<TcpStream, BytesCodec>,
+}
+
+impl SynchroConnection {
+    pub fn connect(addr: SocketAddr) -> Result<Self, Box<std::error::Error>> {
+        let socket = TcpStream::connect(&addr).wait()?;
+        let framed = BytesCodec::new().framed(socket);
+
+        Ok(SynchroConnection {
+            framed,
+        })
+    }
+
+    pub async fn recieve(&mut self) {
+        let mut buffer = BytesMut::new();
+        let mut anticipated_message_length: u16 = 0;
+        while let Some(message) = await!(self.framed.next()) {
+            // Add newly received information to the buffer
+            buffer.unsplit(message.unwrap());
+            
+            // Retrieve message length if we didn't already
+            if anticipated_message_length == 0 {
+                if buffer.len() < 2 {
+                    continue;
+                }
+                
+                anticipated_message_length = buffer.split_to(2).into_buf().get_u16_be();
+
+                println!("message length: {}", anticipated_message_length);
+            }
+
+            // Process the rest of the message if we're sure that we have the entire thing
+            if buffer.len() >= anticipated_message_length as usize {
+                let split_bytes = buffer.split_to(anticipated_message_length as usize);
+                let split_ref = split_bytes.as_ref();
+                let split_buffer = std::io::Cursor::new(split_ref);
+
+                anticipated_message_length = 0;
+                buffer.clear(); // remember to remove this
+            }
+        }
+    }
+}
+
+// fn handle_data(mut data: std::io::Cursor<&[u8]>) {
+//     let has_arguments = data.get_u8() != 0;
+//     println!("{}", has_arguments);
+
+//     let numeric_command = data.get_u8();
+//     let command = Command::from(numeric_command);
+    
+
+//     match command {
+//         Command::Invalid => {
+//             println!("Invalid commnad recieved");
+//         }
+//         Command::Pause => {
+//             let paused = data.get_u8() != 0;
+//             let percent_pos = data.get_f64_be();
+//         }
+//         Command::Seek => {
+//             let percent_pos = data.get_f64_be();
+//             let dragged = data.get_u8() != 0;
+//         }
+//     }
+// }
+
 #[cfg(test)]
 mod tests {
     #[test]
