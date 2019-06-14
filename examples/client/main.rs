c2a9cf35f3681e0fc0d038a919640ae3aaee5d74
Benjamin O <jeep70cp@gmail.com>
2019-06-14 15:27:49 -0400

    Updated client example

--- examples/client/main.rs
+++ examples/client/main.rs
@@ -1,5 +1,3 @@
-#![feature(await_macro, async_await)]
-
 use libsynchro;
 
 use std::net::SocketAddr;
@@ -7,6 +5,6 @@ use std::net::SocketAddr;
 fn main() {
     use libsynchro::SynchroConnection;
     let addr: SocketAddr = format!("127.0.0.1:{}", 32019).parse().unwrap();
-    let conn = SynchroConnection::connect(addr).unwrap();
+    let mut conn = SynchroConnection::new(addr, Box::new(|_|{})).unwrap();
     conn.run();
 }
