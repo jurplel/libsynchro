8f4c95a830705b767da3b9aaeb9f9678c3aac8d5
Benjamin O <jeep70cp@gmail.com>
2019-08-26 21:55:16 -0400

    Updated client example to send name

--- examples/client/main.rs
+++ examples/client/main.rs
@@ -3,7 +3,8 @@ use libsynchro;
 use std::net::SocketAddr;
 use std::env;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let args: Vec<String> = env::args().collect();
     let addr_str;
     if args.len() < 2 {
@@ -14,6 +15,17 @@ fn main() {
 
     use libsynchro::SynchroConnection;
     let addr: SocketAddr = addr_str.parse().unwrap();
-    let mut conn = SynchroConnection::new(addr, Box::new(|_| {})).unwrap();
-    conn.run();
+    let mut conn = SynchroConnection::new_async(addr, Box::new(|_| {})).await.unwrap();
+
+    let (send_job, receive_job) = conn.take_jobs();
+    tokio::spawn(async move {
+        tokio::spawn(receive_job);
+        send_job.await;
+    });
+
+    // Set name for test purposes
+    let name = String::from("Rusty Shackleford");
+    conn.send(libsynchro::Command::SetName {desired_name: name}).unwrap();
+
+    std::thread::park();
 }
